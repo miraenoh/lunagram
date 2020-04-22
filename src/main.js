f27a47d5ad3f4b87ed2e30f64ee28f1192f5c41a
miraenoh <yes_future@naver.com>
2020-04-22 22:30:42 +0900

    Installed and activated firebase and vue-resource

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from "vue";
 import App from "./App.vue";
 
+import * as firebase from "firebase/app";
+import VueResource from "vue-resource";
+
 Vue.config.productionTip = false;
 
+Vue.use(VueResource);
+
+// Initialize firebase
+// TODO Move firebaseconfig to .env
+var firebaseConfig = {
+  apiKey: "AIzaSyAFz7yvyF8EuZ1RNQD8eWCjG2PBWJxUlT8",
+  authDomain: "lunagram-server.firebaseapp.com",
+  databaseURL: "https://lunagram-server.firebaseio.com",
+  projectId: "lunagram-server",
+  storageBucket: "lunagram-server.appspot.com",
+  messagingSenderId: "206176125906",
+  appId: "1:206176125906:web:ef7ccd9cf982d19813be0b",
+  measurementId: "G-QH55N2VG9E",
+};
+firebase.initializeApp(firebaseConfig);
+
 new Vue({
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
