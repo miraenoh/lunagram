55ea0ae37c2240bc1037750743607924f5324927
miraenoh <yes_future@naver.com>
2020-04-26 22:15:36 +0900

    Updated gitignore file and hid firebaseConfig

--- src/main.js
+++ src/main.js
@@ -10,16 +10,7 @@ Vue.use(VueResource);
 
 // Initialize firebase
 // TODO Move firebaseconfig to .env
-var firebaseConfig = {
-  apiKey: "AIzaSyAFz7yvyF8EuZ1RNQD8eWCjG2PBWJxUlT8",
-  authDomain: "lunagram-server.firebaseapp.com",
-  databaseURL: "https://lunagram-server.firebaseio.com",
-  projectId: "lunagram-server",
-  storageBucket: "lunagram-server.appspot.com",
-  messagingSenderId: "206176125906",
-  appId: "1:206176125906:web:ef7ccd9cf982d19813be0b",
-  measurementId: "G-QH55N2VG9E",
-};
+var firebaseConfig = process.env.VUE_APP_FIREBASE_CONFIG;
 firebase.initializeApp(firebaseConfig);
 
 new Vue({
