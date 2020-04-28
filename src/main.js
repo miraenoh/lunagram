d1c86e81b8faf94e779ab418c8b817f5be775066
miraenoh <yes_future@naver.com>
2020-04-29 00:41:51 +0900

    Added TODOs

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import VueResource from "vue-resource";
 Vue.config.productionTip = false;
 
 Vue.use(VueResource);
+// TODO add Vue.http.option.root
+// TODO add other vue.http.option s
 
 // Initialize firebase
 var firebaseConfig = process.env.VUE_APP_FIREBASE_CONFIG;
