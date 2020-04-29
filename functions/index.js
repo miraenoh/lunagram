933a08f53c9f3005370344d2de7ddf396bbf8ef6
miraenoh <yes_future@naver.com>
2020-04-30 04:15:26 +0900

    Added helloWorld function for connection test

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,18 @@
-const functions = require('firebase-functions');
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+// To enable HTTP requests from the front app
+const cors = require("cors")({ origin: true });
+
+// helloWorld function for the connection teste
+exports.helloWorld = functions.https.onRequest((req, res) => {
+  return cors(req, res, () => {
+    res.status(200).json({
+      message: "Hello from Firebase!",
+    });
+  });
+});
